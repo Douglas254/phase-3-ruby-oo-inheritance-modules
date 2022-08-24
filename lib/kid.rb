d056715ce7d09d0d871ca03550474585238ac692
Douglas Obara <douglasobara97@gmail.com>
2022-08-24 16:52:07 +0300

    complete assignment

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+# Kid
+# includes the Dance module
+# has a name
+# extends the MetaDancing module
+
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
